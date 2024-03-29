4b9c3ff046c25959870df466ed51ae08c622c16e
AlanPires01 <70494047+AlanPires01@users.noreply.github.com>
2024-03-28 21:24:12 -0300

    criacao do servidor

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+http.createServer((request, response)=>{
+    response.writeHead(200,{
+        'Content-Type':'text/plain'
+    });
+    response.write('Bem-vindo ao meu servidor');
+    response.end();
+}).listen(1020);
