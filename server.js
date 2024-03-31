b8fadaafabf39a852950e59f75f64061a9bed45f
AlanPires01 <70494047+AlanPires01@users.noreply.github.com>
2024-03-31 07:48:04 -0300

    adicionado logica de requisao do servidor para index.html,style.css, requisicoes de imagem e foi atualizada a pasta assets com novas imagens

--- server.js
+++ server.js
@@ -1,8 +1,42 @@
 const http = require('http');
-http.createServer((request, response)=>{
-    response.writeHead(200,{
-        'Content-Type':'text/plain'
-    });
-    response.write('Bem-vindo ao meu servidor');
-    response.end();
-}).listen(1020);g
+const fs = require('fs');
+const path = require('path');
+const server = http.createServer((request, response)=>{
+    if(request.url =='/'||request.url=='/index.html'){
+        fs.readFile('index.html',(err,arquivo)=>{
+            response.writeHead(200,{'Content-Type':'text/html'})
+            response.write(arquivo)
+            return response.end()
+        })
+    }else if (request.url=='/style.css'){
+        fs.readFile('style.css',(err,arquivo)=>{
+            response.writeHead(200,{'Content-Type':'text/css'});
+            response.write(arquivo);
+            return response.end();
+        })
+    }else if (request.url.startsWith('/assets/')) {
+        const imagePath = path.join(__dirname, request.url);
+        fs.readFile(imagePath, (err, imagem) => {
+            if (err) {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.write('Imagem não encontrada');
+                return response.end();
+            }
+            const imageExtension = path.extname(imagePath);
+            const contentType = `image/${imageExtension.substring(1)}`;
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(imagem);
+        });
+    }
+    
+    // response.writeHead(200,{
+    //     'Content-Type':'text/plain'
+    // });
+    // response.write('Bem-vindo ao meu servidor');
+    // response.end();
+})
+
+const PORT = 3000;
+server.listen(PORT,()=>{
+    console.log("SERVIDOR RODANDO EM http:/localhost:${PORT}");
+})
